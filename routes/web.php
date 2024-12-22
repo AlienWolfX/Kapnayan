b5ab5f460a61542d694e0761659d245e3bd95aff
Allen Cruiz <cruizallen2@gmail.com>
2024-12-22 15:23:51 +0800

    ✨feature: beautify landing page and forgot_password page

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,10 @@ Route::get('/', function () {
     return view('kapnayan');
 });
 
+Route::get('/homepage', function () {
+    return view('homepage');
+});
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
